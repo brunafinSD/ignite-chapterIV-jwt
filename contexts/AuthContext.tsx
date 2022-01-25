c368962f897796a944e57a4bd1b77274212ab53c
Bruna Nunes <bruna.f.n@hotmail.com>
2022-01-25 15:44:05 -0300

    salvando token nos cookies

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, ReactNode, useState } from 'react';
 import { api } from '../services/api';
 import Router from 'next/router';
+import { setCookie } from 'nookies';
 
 type User = {
   email: string;
@@ -37,11 +38,20 @@ export function AuthProvider({ children }: AuthProviderProps) {
       })
 
       const { token, refreshToken, permissions, roles } = response.data;
-      
+
       // PARA ARMAZENAR O TOKEN E REFRESH TOKEN PODEMOS USAR:
       // sessionStorage - se fechar o navegador perde tudo
       // localStorage - não temos acesso do lado do servidor, apenas no browser
       // cookies - armazena informações do browser podendo ser acessado no browser e no servidor
+
+      setCookie(undefined, 'jwt.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 dias
+        path: '/'
+      });
+      setCookie(undefined, 'jwt.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 dias
+        path: '/'
+      });
 
       setUser({
         email,
