e97f3fa217e196e5ddf6d9855ad207cb794ffa25
Bruna Nunes <bruna.f.n@hotmail.com>
2022-01-26 12:05:41 -0300

    criando hook de permissão

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,19 +1,30 @@
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../contexts/AuthContext"
+import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
-export default function Dashboard(){
-  const {user} = useContext(AuthContext);
+export default function Dashboard() {
+  const { user } = useContext(AuthContext);
+
+  const useCanSeeMetrics = useCan({
+    permissions: ['metrics.list'],
+    roles: ['administrator', 'editor']
+  });
 
   useEffect(() => {
     api.get('/me')
-    .then(response => console.log('response dashboard', response))
-    .catch(error => console.log(error))
-  },[]);
-  
-  return <h1>Dashboard: {user?.email}</h1>
+      .then(response => console.log('response dashboard', response))
+      .catch(error => console.log(error))
+  }, []);
+
+  return (
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+      {useCanSeeMetrics && <div>Métricas para quem tem todas as permissões e é administrador ou editor</div>}
+    </>
+  )
 }
 
 // método executado pelo lado do servidor quando o usuário acessar essa página
@@ -21,7 +32,7 @@ export const getServerSideProps = withSSRAuth(async (ctx) => {
   const apiClient = setupAPIClient(ctx);
   const response = await apiClient.get('/me');
   console.log(response.data);
-  
+
   return {
     props: {}
   }
