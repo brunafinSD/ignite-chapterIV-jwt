97fadf77ff8f7db85bb2706e3284ff0f9201bec1
Bruna Nunes <bruna.f.n@hotmail.com>
2022-01-26 10:18:11 -0300

    validando autenticação (server)

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../contexts/AuthContext"
-import { api } from "../services/api";
+import { setupAPIClient } from "../services/api";
+import { api } from "../services/apiClient";
+import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard(){
   const {user} = useContext(AuthContext);
@@ -13,3 +15,16 @@ export default function Dashboard(){
   
   return <h1>Dashboard: {user?.email}</h1>
 }
+
+// método executado pelo lado do servidor quando o usuário acessar essa página
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx);
+  const response = await apiClient.get('/me');
+
+  console.log(response.data);
+  
+  return {
+    props: {}
+  }
+
+});
--- services/api.ts
+++ services/api.ts
@@ -2,75 +2,87 @@ import axios, { AxiosError } from 'axios';
 import { parseCookies, setCookie } from 'nookies';
 import { signOut } from '../contexts/AuthContext';
 
-let cookies = parseCookies();
+
 let isRefreshing = false;
 let failedRequestQueue = [];
 
-export const api = axios.create({
-  baseURL: 'http://localhost:3333',
-  headers: {
-    Authorization: `Bearer ${cookies['jwt.token']}`
-  }
-});
-
-api.interceptors.response.use(response => {
-  return response;
-}, (error: AxiosError) => {
-  if (error.response?.status === 401) {
-    if (error.response.data?.code === 'token.expired') {
-      cookies = parseCookies();
-
-      const { 'jwt.refreshToken': refreshToken } = cookies;
-
-      const originalConfig = error.config;
-
-      if(!isRefreshing){
-        isRefreshing = true;
-
-        api.post('/refresh', {
-          refreshToken,
-        }).then(response => {
-          const { token } = response.data
-          
-          setCookie(undefined, 'jwt.token', token, {
-            maxAge: 60 * 60 * 24 * 30, // 30 dias
-            path: '/'
-          });
-          setCookie(undefined, 'jwt.refreshToken', response.data.refreshToken, {
-            maxAge: 60 * 60 * 24 * 30, // 30 dias
-            path: '/'
-          });
-          
-          api.defaults.headers['Authorization'] = `Bearer ${token}`
-
-          failedRequestQueue.forEach(request => request.onSuccess(token));
-          failedRequestQueue = [];
-
-        }).catch(error => {
-          failedRequestQueue.forEach(request => request.onFailure(error));
-          failedRequestQueue = [];
-
-        }).finally(() => {
-          isRefreshing = false;
-        })
-      }
-
-      return new Promise((resolve, reject) => {
-        failedRequestQueue.push({
-          onSuccess: (token: string) => {
-            originalConfig.headers['Authorization'] = `Bearer ${token}`
-
-            resolve(api(originalConfig));
-          },
-          onFailure: (error: AxiosError) => {
-            reject(error);
-          }
-        })
-      });
-    } else {
-      signOut();
+export function setupAPIClient(ctx = undefined){
+  let cookies = parseCookies(ctx);
+  const api = axios.create({
+    baseURL: 'http://localhost:3333',
+    headers: {
+      Authorization: `Bearer ${cookies['jwt.token']}`
     }
-  }
+  });
+  
+  api.interceptors.response.use(response => {
+    return response;
+  }, (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      if (error.response.data?.code === 'token.expired') {
+        cookies = parseCookies(ctx);
+  
+        const { 'jwt.refreshToken': refreshToken } = cookies;
+  
+        const originalConfig = error.config;
+  
+        if(!isRefreshing){
+          isRefreshing = true;
+  
+          api.post('/refresh', {
+            refreshToken,
+          }).then(response => {
+            const { token } = response.data
+            
+            setCookie(ctx, 'jwt.token', token, {
+              maxAge: 60 * 60 * 24 * 30, // 30 dias
+              path: '/'
+            });
+            setCookie(ctx, 'jwt.refreshToken', response.data.refreshToken, {
+              maxAge: 60 * 60 * 24 * 30, // 30 dias
+              path: '/'
+            });
+            
+            api.defaults.headers['Authorization'] = `Bearer ${token}`
+  
+            failedRequestQueue.forEach(request => request.onSuccess(token));
+            failedRequestQueue = [];
+  
+          }).catch(error => {
+            failedRequestQueue.forEach(request => request.onFailure(error));
+            failedRequestQueue = [];
+  
+            // executar somente se estiver no lado do cliente
+            if(process.browser){
+              signOut();
+            }
+  
+          }).finally(() => {
+            isRefreshing = false;
+          })
+        }
+  
+        return new Promise((resolve, reject) => {
+          failedRequestQueue.push({
+            onSuccess: (token: string) => {
+              originalConfig.headers['Authorization'] = `Bearer ${token}`
+  
+              resolve(api(originalConfig));
+            },
+            onFailure: (error: AxiosError) => {
+              reject(error);
+            }
+          })
+        });
+      } else {
+        if(process.browser){
+          signOut();
+        }
+      }
+    }
+  
+    return Promise.reject(error);
+  });
 
-  return Promise.reject(error);
-});
+  return api;
+}
